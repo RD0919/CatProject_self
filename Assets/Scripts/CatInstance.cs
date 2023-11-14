a9a5cf5ee2d46643a7547b30edcfa2ddde710da1
RD <a01050353566@gmail.com>
2023-11-14 16:30:06 +0900

    Initial commit

---
+++ Assets/Scripts/CatInstance.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatInstance : MonoBehaviour
+{
+    public CatBehavior m_CatBehavior;
+    public CatMovement m_CatMovement;
+
+
+}
